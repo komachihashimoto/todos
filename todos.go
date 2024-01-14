f19ef2df4523e0755c3908ee1ca7fe952e800d68
komachihashimoto <joujinameko@gmail.com>
2024-01-14 21:03:52 +0900

    todoアプリ化１

--- todos.go
+++ todos.go
@@ -4,12 +4,21 @@ import (
     "github.com/gin-gonic/gin"
 )
 
+type Todo struct {
+    Title string `json:"title"`
+    Done  bool   `json:"done"`
+}
+
+var todos = []Todo{}
+
 func main() {
     router := gin.Default()
-    router.LoadHTMLGlob("./index.html")
+    router.LoadHTMLGlob("templates/index.html")
 
     router.GET("/", func(ctx *gin.Context) {
-        ctx.HTML(200, "index.html", gin.H{})
+        ctx.HTML(200, "index.html", gin.H{
+            "todos": todos,
+        })
     })
 
     router.Run()
